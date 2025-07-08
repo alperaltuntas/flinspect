b7f5c0921895aab6229a37e0e767c73015bcba96
alperaltuntas <alperaltuntas@gmail.com>
2025-07-07 23:59:32 -0600

    add a utils module

---
+++ flinspect/utils.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+def level(line):
+    """
+    Determine the level of indentation based on the number of leading |.
+    """
+    res = 0
+    for c in line:
+        if c == '|':
+            res += 1
+        elif c == ' ':
+            continue
+        else:
+            return res
+
+
